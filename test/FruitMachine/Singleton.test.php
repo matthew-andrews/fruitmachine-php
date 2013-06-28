acb10527b8be2f17c114054cbce1d6e866dcf155
Matthew Andrews <matthew@andrews.eu.com>
2013-06-28 01:35:18 +0100

    underscoring test name

--- test/FruitMachine/Singleton.test.php
+++ test/FruitMachine/Singleton.test.php
@@ -2,7 +2,7 @@
 
 class SingletonTest extends \PHPUnit_Framework_TestCase {
 
-  public function testSingletonIsInstanceOfFruitMachine() {
+  public function test_singleton_is_instance_of_FruitMachine() {
     $singleton = \FruitMachine\Singleton::getInstance();
     $this->assertTrue($singleton instanceof \FruitMachine\FruitMachine);
   }
