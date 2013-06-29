429c0b58d40e18a80d4c1ffc09a236342fb015b0
Matthew Andrews <matthew@andrews.eu.com>
2013-06-30 00:24:37 +0100

    Finish toHTML tests

--- test/FruitMachine/Module.toHTML.test.php
+++ test/FruitMachine/Module.toHTML.test.php
@@ -36,4 +36,31 @@ class ModuleToHTMLTest extends \PHPUnit_Framework_TestCase {
     $this->assertContains($appleHtml, $layoutHtml);
   }
 
+  public function test_should_print_the_child_html_by_id_if_no_slot_is_found() {
+    $apple = Singleton::getInstance()->create('apple', array("id" => 1));
+    $layout = Singleton::getInstance()->create('layout', array(
+      "children" => array($apple)
+    ));
+
+    $appleHtml = $apple->toHTML();
+    $layoutHtml = $layout->toHTML();
+
+    $this->assertContains($appleHtml, $layoutHtml);
+  }
+
+  public function test_should_fallback_to_printing_children_by_id_if_no_slot_is_present() {
+    $layout = Singleton::getInstance()->create('layout', array(
+      "children" => array(
+        array(
+          "module" => 'apple',
+          "id" => 1
+        )
+      )
+    ));
+
+    $layoutHtml = $layout->toHTML();
+
+    $this->assertContains('apple', $layoutHtml);
+  }
+
 }
